e3cee3b044f773bebaebb91ec13f7d1e1ab0b19f
Ly Quoc Hung <s3426511@rmit.edu.vn>
2014-07-01 10:37:48 +0700

    Initial commit

---
+++ app/src/androidTest/java/org/rmit/hung/thingsdo/ApplicationTest.java
@@ -0,0 +1,28 @@
+/*
+ * ApplicationTest.java
+ * Task Management Application
+ * 1, COSC2543 - Mobile Application Development
+ * RMIT International University Vietnam
+ *
+ * Copyright 2014 Ly Quoc Hung (s3426511)
+ *
+ * Refer to the NOTICE file in the root of the source tree for
+ * acknowledgements of third party works used in this software.
+ *
+ * Date created:       01/07/2014
+ * Date last modified: 01/07/2014
+ */
+
+package org.rmit.hung.myapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+	public ApplicationTest() {
+		super(Application.class);
+	}
+}
